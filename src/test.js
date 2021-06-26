cf63f3a017b8e2c9e94bc3417997bae33447a2d5
Sateesh Peetha <sateesh.peetha@gmail.com>
2021-06-26 23:28:02 +0530

    added logic to run simple test

--- src/test.js
+++ src/test.js
@@ -2,7 +2,21 @@ const { handler } = require('./checkout');
 console.log("in test function");
 (async function f() {
     process.env.stage = 'local';
-    console.log("came here");
-    const result = await handler({ body: { customerid: "1" } });
+    
+    console.log("Testing for customer default no deals");
+    let result = await handler({ body: { customerid: "66" } });
     console.log(result);
+
+    console.log("Testing for customer UEM Sunrise");
+    result = await handler({ body: { customerid: "1" } });
+    console.log(result);
+
+    console.log("Testing for customer Sime Darby Property Bhd.");
+    result = await handler({ body: { customerid: "2" } });
+    console.log(result);
+
+    console.log("Testing for customer  IGB Berhad");
+    result = await handler({ body: { customerid: "3" } });
+    console.log(result);
+
 })();
